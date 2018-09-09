88562569be081dc34233260900d88302c2721011
Bas Maat <bjmaat@gmail.com>
2018-09-09 13:23:47 +0200

    Completed assingment 6

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,4 +1,13 @@
+def player(numeral):
+    x = 0
+    while x < int(numeral):
+        import simpleaudio.functionchecks as fc
+        fc.LeftRightCheck.run()
+        x += 1
+    if x >= int(numeral):
+        print("Thank you for using my program")
+
 username = input("Please tell us your name: ")
 print("Hello", username)
-import simpleaudio.functionchecks as fc
-fc.LeftRightCheck.run()
+num = input("How many times play the sample: ")
+player(num)
