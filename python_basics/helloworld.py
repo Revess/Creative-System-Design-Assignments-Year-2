4bf57335a3e44d7ce445b6645b4101c5e21d133d
Bas Maat <revessyt@gmail.com>
2018-09-18 10:11:04 +0200

    Added a new version to play a file

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -1,13 +1,24 @@
+#-----Imports------#
+import simpleaudio as sa
+import threading
+
+#-----Funtions-----#
+def playFile(file_path):
+    wave_obj = sa.WaveObject.from_wave_file(file_path)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
 def player(numeral):
     x = 0
     while x < int(numeral):
-        import simpleaudio.functionchecks as fc
-        fc.LeftRightCheck.run()
         x += 1
+        playFile("CowBell.wav")
+        print(x)
     if x >= int(numeral):
         print("Thank you for using my program")
 
-username = input("Please tell us your name: ")
-print("Hello", username)
+#-----Main--------#
+username_obj = input("Please tell us your name: ")
+print("Hello", username_obj)
 num = input("How many times play the sample: ")
 player(num)
