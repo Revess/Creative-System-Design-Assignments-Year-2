ae8865294a3dd23f24bace880ff19524de5dba43
Bas Maat <bjmaat@gmail.com>
2018-10-01 21:04:57 +0200

    De gegeven code vervijnd (zie commends). Al wat geprobeerd met Threading, maar nog niet tot resultaat gekomen (zie Threading_test)

---
+++ blok2a/Threading_test.py
@@ -0,0 +1,172 @@
+import simpleaudio as sa
+import threading
+from threading import Thread
+import time
+
+#----Objects----#
+#Machine on or off
+running = True
+#State of machine
+state = 'main'
+#Path to file
+filePath = "CowBell.wav"
+#Bpm in ms
+bpm = 0.5
+#Amount of playtimes
+numOfPlays = 5
+#List of possible rhythms
+rhythm_obj_lst = [1, 2, 4 ,8 ,16]
+#List that contains the rhythm
+rhythm_lst = [1, 1, 1, 1,]
+#Timestamps
+timestamps = [0, 0.5, 1.0 ,1.5]
+#playerState
+playerState = 'pause'
+#The time of starting the code
+startTime = time.time()
+#Object file_path
+samples = [sa.WaveObject.from_wave_file("../python_basics/CowBell.wav")]
+#When started will it keep playing?
+keepPlaying = True
+
+#----Functions----#
+def playFile():
+    samples[0].play()
+
+def converter(lst):
+    stamps = []
+    ms_lst = []
+    str_lst = lst.split()
+    for i in str_lst:
+        if i == '|':
+            str_lst.remove(i)
+    float_lst = [float(i) for i in str_lst]
+    for i in float_lst:
+        for x in rhythm_obj_lst:
+            if i == x:
+                i = i/4
+                i = bpm/i
+                ms_lst.append(i)
+    prev = -ms_lst[0]
+    for i in ms_lst:
+        stamps.append(i + prev)
+        prev += i
+    return stamps
+
+def play():
+    global playerState
+    global startTime
+    global keepPlaying
+    timestamps_lst = timestamps
+    timestamp = timestamps_lst.pop(0)
+    while keepPlaying:
+        if playerState == 'play':
+            currentTime = time.time()
+            if(currentTime - startTime >= timestamp):
+                # play sample
+                playFile()
+                if playerState == 'playOnce':
+                    if timestamps:
+                        timestamp = timestamps.pop(0)
+                    else:
+                        keepPlaying = False
+                if playerState == 'stop':
+                    keepPlaying = False
+                while playerState == 'pause':
+                    time.sleep(0.001)
+            else:
+                time.sleep(0.001)  
+
+def stating():
+    global running, state, filePath, bpm, numOfPlays, rhythm_lst, rhythm_obj_lst, playerState, timestamps
+    #Loop#
+    while running:
+    ####Setup####
+        if state == 'main':
+            state = input(">> ")
+
+    ####Fundamentals####
+    #---Help
+        elif state == 'help':
+            print("The following commands can be used: \ncurrentFile   play \nexit()        rhythm \ngetFile       tempo \nhelp          testFile \nnumOfPlays    testSequencer")
+            state = 'main'
+
+    ####Commands####
+    ##----currentFile----##
+        elif state == 'currentFile':
+            print(filePath)
+            state = 'main'
+    ##----play----##
+        elif state == 'play':
+            print("Also try: playOnce, stop and pause")
+            playerState = 'play'
+            state = 'main'
+    ##----playOnce----##
+        elif state == 'playOnce':
+            playerState = 'playOnce'
+            state = 'main'
+    ##----stop----##
+        elif state == 'stop':
+            playerState = 'stop'
+            state = 'main'
+    ##----pause----##
+        elif state == 'pause':
+            playerState = 'pause'
+            state = 'main'
+    ##----rhythm----##
+        elif state == 'rhythm':
+            print("Would you like to view or create the rhythm?")
+            state = input(">>> ")
+            if state == 'view':
+                print("The current rhythmSequence is: " + str(rhythm_lst))
+                state = 'main'
+            elif state == 'create':
+                print("Give your desired rhythm: ")
+                rhythm_lst = []
+                timestamps = []
+                rhythm_lst = input(">>> ")
+                timestamps = converter(rhythm_lst)
+                state = 'main'
+            else:
+                state = 'main'
+    ##----getFile----##
+        elif state == 'getFile':
+            filePath = input(">>> ")
+            state = 'main'
+    ##----tempo----##
+        elif state == 'tempo':
+            print("Would you like to view or set the tempo?")
+            state = input(">>> ")
+            if state == 'view':
+                print("The current tempo is set to " + str(60/bpm) + " BPM")
+                state = 'main'
+            elif state == 'set':
+                print("Give your desired tempo in BPM:")
+                bpm = float(input(">>> "))
+                print("The current tempo has been set to " + str(bpm) + " BPM")
+                bpm = 60/bpm
+                state = 'main'
+    ##----testFile----##
+        elif state == 'testFile':
+            print("Starting file")
+            playFile()
+            print("Done playing")
+            state = 'main'
+    ##----numOfPlays----##
+        elif state == 'numOfPlays':
+            numOfPlays = int(input(">>> "))
+            if numOfPlays > 20:
+                print("This number is too high")
+            else:
+                state = 'main'
+    #---Exit message to exit program
+        elif state == 'exit()':
+            exit()
+    #---Invalid input
+        else:
+            print("Unkown command, to see the full list of commands use help")
+            state = 'main'
+    
+if __name__ == '__main__':
+    Thread(target = stating).start()
+    Thread(target = play).start()
---
+++ blok2a/sequencerV2.py
@@ -0,0 +1,77 @@
+##--Imports--##
+import simpleaudio as sa
+import time
+import random
+
+##--Objects--##
+samples = [sa.WaveObject.from_wave_file("../python_basics/CowBell.wav")]
+bpm = 120
+tempo = 60 / bpm
+keepPlaying = True
+firstRun = True
+timestamps = [0]
+rhythm = []
+rhythm_obj_lst = [1, 2, 4 ,8 ,16]
+timestamps = []
+plays = 0
+
+##--Functions--##
+#Convert a rhythm string to timestamps
+def convert(string):
+    stamps = []
+    a = 0
+    ms_lst = []
+    str_lst = string.split()
+    float_lst = [float(i) for i in str_lst]
+    for i in float_lst:                         #Convert the given list to the miliseconds by comparing the given values to the rhythm list
+        for x in rhythm_obj_lst:
+            if i == x:
+                i = i/4                         #Brings the notes back to whole numbers (4 = quarter. 4/4 = 1. 8 = eight. 8/4 = 2 etc.)
+                i = tempo/i                     #Over here the tempo will be devided by the number, so quarter will be 0.5, an eigth (tempo/2) will be 0.25.
+                ms_lst.append(i)                #Add them to a new list
+    ms_lst.insert(0, a)                         #Append a 0 to the list to make sure first sample always starts right at the beginning
+    p = 0
+    x = 0
+    for i in ms_lst:                            #Convert the milisecond list to a timestamp list by adding them together in seperate spaces
+        if i == 0:
+            stamps.append(i)
+            p += 1
+        else:
+            x += i
+            stamps.append(x)
+            p += 1
+    return stamps
+
+#Play the given list
+def player(lst):
+    global keepPlaying, plays
+    keepPlaying = True                          #Keeps the loop going until done
+    update = True                               #Makes sure the code is updating the time if the given time is not met
+    while keepPlaying:
+        if plays > 0:                           #Checks if the loop has to stop
+            startTime = time.time()             #Updates the start time every new loop
+            for i in lst:                       #Walks us through the timestamps list
+                update = True                   #Update sets to true every time the loop restarts
+                while update:
+                    currentTime = time.time()   #Update the current time for the coming equation
+                    if(currentTime - startTime >= i):
+                        samples[0].play()       #Play the given sample out of the file
+                        update = False          #When the sample is played the update will set to false, this way the next time stamp will be used
+            plays -= 1                          #Substract one from the asked loops
+        elif plays <= 0:                        #if previous statement doesn't go in effect this one will stop the loop
+            keepPlaying = False
+    print("Done playing")
+
+def shuffle_lst():
+    return        
+
+##--Main--##
+def main():
+    global bpm, tempo, rhythm, timestamps, plays
+    bpm = float(input("What bpm would you like to use? \n>>> "))
+    tempo = 60 / bpm
+    rhythm = convert(input("What rhythm would you like to play? \n>>> "))
+    plays = int(input("How many times would you like the rhythm to be played? \n>>> "))
+    timestamps = rhythm.copy()
+    player(timestamps)
+main()
